327ff4136900b5505942d1045241497ac52ecd5e
Tam Shu Chuen <shuchuen109@gmail.com>
2016-09-14 02:34:37 +0800

    comp3021 lab2 task

---
+++ src/base/NoteBook.java
@@ -0,0 +1,54 @@
+package base;
+
+import java.util.ArrayList;
+
+public class NoteBook {
+
+	private ArrayList<Folder> folders;
+	
+	public NoteBook(){
+		folders = new ArrayList<Folder>();
+	}
+	
+	public boolean createTextNote(String folderName, String title) {
+		TextNote note = new TextNote(title);
+		return insertNote(folderName, note);
+	}
+	
+	public boolean createImageNote(String folderName, String title) {
+		ImageNote note = new ImageNote(title);
+		return insertNote(folderName, note);
+	}
+		
+	public ArrayList<Folder> getFolders() {
+		return this.folders;
+	}
+	
+	private boolean insertNote(String folderName, Note note){
+		
+		Folder f = null;
+		
+		for (Folder f1 : folders) {
+			if(f1.equals(new Folder(folderName))){
+				f = f1;
+			}
+		}
+		if (f == null) {
+			f = new Folder(folderName);
+			this.folders.add(f);
+		}
+		
+		for (Note n : f.getNotes()) {
+			if(n.equals(note)){
+				System.out.println("Create note " + note.getTitle() 
+				+ " under folder " + folderName + " failed");
+				return false;
+			}
+		}
+		
+		f.addNote(note);
+		return true;
+		
+	}
+	
+}
