909c88482f62cde58dd4dd9319846af7aadffb36
Tam Shu Chuen <shuchuen109@gmail.com>
2016-09-14 10:57:34 +0800

    comp3021 lab2 task

--- src/base/ImageNote.java
+++ src/base/ImageNote.java
@@ -9,30 +9,4 @@ public class ImageNote extends Note{
 	public ImageNote(String title) {
 		super(title);
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((image == null) ? 0 : image.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ImageNote other = (ImageNote) obj;
-		if (image == null) {
-			if (other.image != null)
-				return false;
-		} else if (!image.equals(other.image))
-			return false;
-		return true;
-	}
-
 }
